a68f68eb6a9abe5627054a1fc25851dfbde31430
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-06-06 15:07:24 +0300

    finish HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <main
-      className="flex-grow bg-no-repeat bg-cover"
+      className="flex flex-grow justify-start bg-no-repeat bg-cover"
       style={{ backgroundImage: `url(${bg})` }}
     >
-      <div className="flex items-center mx-auto max-w-[1440px] px-16">
+      <div className="mx-auto max-w-[1440px] px-16 w-full flex items-center">
         <div>
           <h1 className="text-5xl leading-8 font-semibold text-inputs">Campers of your dreams</h1>
           <p className="mt-4 text-2xl text-inputs">You can find everything you want in our catalog</p>
